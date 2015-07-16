040805ad014dfdc18c8aa50fb42f45838e3a8724
Gabe Scholz <gabe@brewhouse.io>
2015-07-16 16:10:32 -0700

    some small changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import 'babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends React.Component {
-  render () {
-    return (
-      <div>Hello</div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('#container'));
